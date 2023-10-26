6db3a2398105e45d16ec902e1a90f634492cccf8
Sagida <138651589+Sagida07@users.noreply.github.com>
2023-10-26 09:41:22 +0600

    Update Magic.java

--- src/kg/geeks/game/players/Magic.java
+++ src/kg/geeks/game/players/Magic.java
@@ -3,21 +3,18 @@ package kg.geeks.game.players;
 import kg.geeks.game.players.Boss;
 import kg.geeks.game.players.Hero;
 
-public class Magic extends Hero {
-    private int increaseDamage;
+public class Magic extends Hero{
 
-
+    private int boostDamage = 20;
     public Magic(int health, int damage, String name) {
-        super(health, damage, SuperAbility.BOOST, name);
-        this.increaseDamage = increaseDamage;
+        super(health, damage, name, SuperAbility.BOOST);
     }
 
     @Override
     public void applySuperPower(Boss boss, Hero[] heroes) {
-        for (Hero hero: heroes) {
-            if(hero.getAbility() != SuperAbility.REVIVE) {
-                hero.setDamage(hero.getDamage() + this.increaseDamage);
-            }
+        for (Hero hero: heroes){
+            hero.setDamage(hero.getDamage() + this.boostDamage);
+            System.out.println("Magic" + " boosted " + hero.getName() + " boosted " + boostDamage);
         }
     }
 }
